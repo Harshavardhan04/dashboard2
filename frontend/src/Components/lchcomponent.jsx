188385363fe3a47053b019ba8fcc39dd0b0af24f
Harsh <63057241+Harshavardhan04@users.noreply.github.com>
2024-07-21 18:39:19 +0100

    Update lchcomponent.jsx

--- frontend/src/Components/lchcomponent.jsx
+++ frontend/src/Components/lchcomponent.jsx
@@ -1,44 +1,29 @@
-import React, { useState, useEffect, useRef } from 'react';
-import CurrencySelector from '../../Components/xva/CurrencySelector';
-import DateSelectors from '../../Components/generic/DateSelectors';
-import Table from '../../Components/generic/GenericTable';
-import GraphComponent from '../../Components/xva/GraphComponent';
+import React, { useEffect, useRef, useState } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import HighchartsBoost from 'highcharts/modules/boost';
+import HighchartsExporting from 'highcharts/modules/exporting';
+import HighchartsAnnotations from 'highcharts/modules/annotations';
+import HighchartsMore from 'highcharts/highcharts-more';
 import { formatNumber } from '../../Utils/Utils';
-import '../../Styles/Graph.css';
+import ChartDownload from '../../Components/xva/ChartDownload';
 
-const LCHNotional = () => {
-  const [compareWithTarget, setCompareWithTarget] = useState(false);
-  const [selectedCurrencies, setSelectedCurrencies] = useState([
-    { value: 'AUD', label: 'AUD' },
-    { value: 'EUR', label: 'EUR' },
-    { value: 'GBP', label: 'GBP' },
-    { value: 'JPY', label: 'JPY' },
-    { value: 'USD', label: 'USD' },
-  ]);
-  const [startDate, setStartDate] = useState(new Date('2018-06-01'));
-  const [endDate, setEndDate] = useState(new Date('2024-06-25'));
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const [summary, setSummary] = useState('');
+HighchartsBoost(Highcharts);
+HighchartsExporting(Highcharts);
+HighchartsAnnotations(Highcharts);
+HighchartsMore(Highcharts);
 
-  const latestSummaryRef = useRef('');
+const GraphComponent = ({
+  startDate,
+  endDate,
+  selectedCurrencies,
+  isDarkMode,
+  data,
+  compareWithTarget,
+  setCompareWithTarget
+}) => {
   const chartRef = useRef(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('/xva_data_lch_notional');
-        const result = await response.json();
-        setData(result);
-        setLoading(false);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-        setLoading(false);
-      }
-    };
-    fetchData();
-  }, []);
+  const latestSummaryRef = useRef('');
 
   const getFilteredData = () => {
     return data.filter((d) => {
@@ -47,91 +32,229 @@ const LCHNotional = () => {
     });
   };
 
-  const filteredData = getFilteredData();
-
-  const generateColumns = () => {
-    const baseColumns = [
-      { field: 'id', headerName: 'ID', flex: 0.5, minWidth: 100 },
-      { field: 'date', headerName: 'Date', flex: 1, minWidth: 120 },
-      { field: 'target', headerName: 'Target', flex: 1.5, minWidth: 150 }
-    ];
-
-    const currencyColumns = selectedCurrencies.map((currency) => ({
-      field: currency.value.toLowerCase(),
-      headerName: currency.label,
-      flex: 1.5,
-      minWidth: 150,
+  const getData = () => {
+    const filteredData = getFilteredData();
+    const compareData = selectedCurrencies.map((currency) => ({
+      name: currency.value,
+      data: filteredData.map((d) => [
+        new Date(d.Date).getTime(),
+        d[currency.value] !== undefined ? d[currency.value] : null
+      ]),
+      color: getCurrencyColor(currency.value),
+      marker: { enabled: false },
+      boostThreshold: 1,
     }));
 
-    return [
-      ...baseColumns,
-      ...currencyColumns,
-      { field: 'total', headerName: 'Total', flex: 1.5, minWidth: 150 },
-    ];
+    const totalLine = {
+      name: "Total",
+      data: filteredData.map((d) => [new Date(d.Date).getTime(), d.Total]),
+      color: isDarkMode ? "#ffffff" : "#000000",
+      marker: { enabled: false },
+      zIndex: 1,
+      boostThreshold: 1,
+    };
+
+    const target = {
+      name: "Target",
+      data: filteredData.map((d) => [new Date(d.Date).getTime(), d.Target]),
+      color: "#007bff",
+      marker: { enabled: false },
+      zIndex: 1,
+      boostThreshold: 1,
+    };
+
+    const shadeData = {
+      name: "Shaded Area",
+      data: filteredData.map((d) => ({
+        x: new Date(d.Date).getTime(),
+        low: Math.min(d.Total, d.Target),
+        high: Math.max(d.Total, d.Target),
+      })),
+      type: "arearange",
+      lineWidth: 0,
+      color: "#228B22",
+      fillOpacity: 0.3,
+      zIndex: 0,
+      marker: { enabled: false },
+      boostThreshold: 1,
+    };
+
+    return compareWithTarget ? [...compareData, totalLine, target, shadeData] : [...compareData, totalLine, target];
   };
 
-  const generateRows = () => {
-    return filteredData.map((d, index) => {
-      const rowData = {
-        id: index + 1,
-        date: d.Date,
-        target: formatNumber(d.Target),
-        total: formatNumber(d.Total),
-      };
-      selectedCurrencies.forEach((currency) => {
-        rowData[currency.value.toLowerCase()] = formatNumber(d[currency.value]);
+  const getCurrencyColor = (currency) => {
+    const colors = {
+      AUD: "#ff6600",
+      EUR: "#28a745",
+      GBP: "#dc3545",
+      JPY: "#343a40",
+      USD: "#ffc107",
+      BRL: "#f16767",
+      CAD: "#00a5cf",
+      CHF: "#69c267",
+      CLP: "#9a67c2",
+      CNY: "#d3a1c5",
+      CZK: "#305d7b",
+      DKK: "#9e68a2",
+      HKD: "#778899",
+      HUF: "#7ccc67",
+      INR: "#2e4053",
+      KRW: "#5f9ea0",
+      MXN: "#4b0082",
+      NOK: "#ec704a",
+      NZD: "#9b59b6",
+      PLN: "#ff6f61",
+      SEK: "#00a99d",
+      SGD: "#ff6f91",
+      THB: "#1abc9c",
+      TWD: "#6495ed",
+      ZAR: "#dd5182",
+    };
+
+    return colors[currency] || "#000000";
+  };
+
+  const updateSummary = (points) => {
+    if (points) {
+      const totalPoint = points.find((p) => p.series.name === 'Total');
+      const targetPoint = points.find((p) => p.series.name === 'Target');
+
+      if (totalPoint && targetPoint) {
+        const totalValue = totalPoint.y;
+        const targetValue = targetPoint.y;
+        const difference = totalValue - targetValue;
+        const totalBreakdown = selectedCurrencies.map((currency) => {
+          const point = points.find((p) => p.series.name === currency.value);
+          return `<strong>${currency.value}:</strong> ${point ? formatNumber(point.y) : 'N/A'}`;
+        }).join('<br>');
+
+        const summaryHTML = `
+          <strong>Total:</strong> ${formatNumber(totalValue)}<br>
+          <strong>Target:</strong> ${formatNumber(targetValue)}<br>
+          <strong>Difference:</strong> ${formatNumber(difference)}<br>
+          <strong>Breakdown of Selected Currencies:</strong><br>${totalBreakdown}
+        `;
+
+        latestSummaryRef.current = summaryHTML;
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (chartRef.current) {
+      chartRef.current.chart.update({
+        series: getData(),
+        tooltip: {
+          formatter: function () {
+            updateSummary(this.points);
+            return latestSummaryRef.current;
+          }
+        }
       });
-      return rowData;
-    });
-  };
+    }
+  }, [startDate, endDate, selectedCurrencies, data, isDarkMode, compareWithTarget]);
 
-  const rows = generateRows();
-  const columns = generateColumns();
+  const chartOptions = {
+    chart: {
+      type: 'line',
+      zoomType: 'x',
+      backgroundColor: isDarkMode ? '#2e2e2e' : '#fafafa',
+      plotBorderWidth: 1,
+      plotBorderColor: isDarkMode ? '#444444' : '#cccccc',
+    },
+    title: {
+      text: 'LCH Notional | Time Series',
+      style: {
+        color: isDarkMode ? '#ffffff' : '#000000',
+        fontWeight: 'bold',
+      },
+    },
+    xAxis: {
+      type: 'datetime',
+      title: {
+        text: 'Date',
+        style: {
+          color: isDarkMode ? '#cccccc' : '#000000',
+          fontWeight: 'bold',
+        },
+      },
+      labels: {
+        style: {
+          color: isDarkMode ? '#cccccc' : '#000000',
+        },
+      },
+      lineColor: isDarkMode ? '#444444' : '#cccccc',
+      tickColor: isDarkMode ? '#444444' : '#cccccc',
+    },
+    yAxis: {
+      title: {
+        text: 'Notional (USD)',
+        style: {
+          color: isDarkMode ? '#cccccc' : '#000000',
+          fontWeight: 'bold',
+        },
+      },
+      labels: {
+        style: {
+          color: isDarkMode ? '#cccccc' : '#000000',
+        },
+      },
+      gridLineColor: isDarkMode ? '#444444' : '#cccccc',
+    },
+    legend: {
+      layout: 'horizontal',
+      align: 'center',
+      verticalAlign: 'bottom',
+      itemStyle: {
+        color: isDarkMode ? '#ffffff' : '#000000',
+        fontWeight: 'bold',
+      },
+    },
+    tooltip: {
+      shared: true,
+      backgroundColor: isDarkMode ? '#333333' : '#ffffff',
+      borderColor: isDarkMode ? '#444444' : '#cccccc',
+      style: {
+        color: isDarkMode ? '#ffffff' : '#000000',
+      },
+      formatter: function () {
+        let s = `<strong>Date:</strong> ${Highcharts.dateFormat('%A, %b %e, %Y', this.x)}<br>`;
+        s += `<strong>Total:</strong> ${Highcharts.numberFormat(this.points.find(p => p.series.name === 'Total').y, 0)}<br>`;
+        s += `<strong>Target:</strong> ${Highcharts.numberFormat(this.points.find(p => p.series.name === 'Target').y, 0)}<br>`;
+        s += `<strong>Difference:</strong> ${Highcharts.numberFormat(this.points.find(p => p.series.name === 'Total').y - this.points.find(p => p.series.name === 'Target').y, 0)}<br>`;
+        s += '<strong>Breakdown of Selected Currencies:</strong><br>';
+
+        this.points.forEach(point => {
+          if (point.series.name !== 'Total' && point.series.name !== 'Target') {
+            s += `<strong>${point.series.name}:</strong> ${Highcharts.numberFormat(point.y, 0)}<br>`;
+          }
+        });
+
+        return s;
+      }
+    },
+    series: getData(),
+  };
 
   return (
     <>
-      <div className="graph-container">
-        <h2 className="graph-title">LCH Notional | Time Series</h2>
-        <div className="selectors-container">
-          <DateSelectors
-            startDate={startDate}
-            setStartDate={setStartDate}
-            endDate={endDate}
-            setEndDate={setEndDate}
-          />
-          <div className="currency-selector">
-            <CurrencySelector
-              options={options}
-              selectedCurrencies={selectedCurrencies}
-              setSelectedCurrencies={setSelectedCurrencies}
-            />
-          </div>
-        </div>
-        <div className="main-panel">
-          <GraphComponent
-            startDate={startDate}
-            endDate={endDate}
-            selectedCurrencies={selectedCurrencies}
-            isDarkMode={isDarkMode}
-            data={filteredData}
-            compareWithTarget={compareWithTarget}
-            setCompareWithTarget={setCompareWithTarget}
-            summary={summary}
-            setSummary={setSummary}
-          />
-        </div>
+      <div>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => setCompareWithTarget(!compareWithTarget)}
+        >
+          {compareWithTarget ? 'Disable Target Comparison' : 'Enable Target Comparison'}
+        </Button>
+        <ChartDownload chartRef={chartRef} />
       </div>
-
-      <div className="table-container">
-        <h2 className="table-title">LCH Notional | Summary Table</h2>
-        <div className="data-grid-container">
-          <div className="data-grid-wrapper">
-            <Table rows={rows} columns={columns} loading={loading} />
-          </div>
-        </div>
-      </div>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={chartOptions}
+        ref={chartRef}
+      />
     </>
   );
 };
 
-export default LCHNotional;
+export default GraphComponent;
